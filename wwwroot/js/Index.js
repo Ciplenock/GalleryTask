5a1ef4e5b41715cf96343f877dac4dc415e937ae
Андрей Орлов <neumni12@gmail.com>
2023-03-15 03:33:12 +0300

    Add project files.

---
+++ wwwroot/js/Index.js
@@ -0,0 +1,14 @@
+﻿var imgId = 0;
+function windowImgRight() {
+    imgId = imgId + 1;
+    if (imgId >= 6) imgId = 0;
+    document.getElementById(9).src = document.getElementById(imgId).src;
+}
+function windowImgLeft() {
+    imgId = imgId - 1;
+    if (imgId <= -1) imgId = 5;
+    document.getElementById(9).src = document.getElementById(imgId).src;
+}
+function downImgClick() {
+    document.addEventListener('click', e => document.getElementById(9).src = e.target.src)
+}
