1b60b774dd15727c1432805a845b677477a6a786
Андрей Орлов <neumni12@gmail.com>
2023-03-20 02:09:36 +0300

    Доделал пк версию

--- wwwroot/js/Index.js
+++ wwwroot/js/Index.js
@@ -1,24 +1,42 @@
-﻿var imgId = 0;
+﻿var previmgId = 0;
+var imgId = 0;
+
+document.getElementById(imgId).style.filter = 'blur(0)';
 
 function windowImgRight() {
+    previmgId = imgId;
     imgId = imgId + 1;
+
     if (imgId >= 6) imgId = 0;
+    changeBlur();
+    
     document.getElementById(9).src = document.getElementById(imgId).src;
 }
 function windowImgLeft() {
+
+    previmgId = imgId;
     imgId = imgId - 1;
     if (imgId <= -1) imgId = 5;
+    changeBlur();
     document.getElementById(9).src = document.getElementById(imgId).src;
-    console.log(imgId)
 }
 
+function changeBlur() {
+    document.getElementById(previmgId).style.filter = 'blur(1px)';  
+    document.getElementById(imgId).style.filter = 'blur(0)';
+    
+
+}
 const images = Array.from(document.querySelectorAll('.down_img'));
 
-for (const image of images) {
-    image.addEventListener('click', e => {   
-        imgId = images.indexOf(image);
 
-        document.getElementById(imgId).style.filter = 'blur(0)';
+for (const image of images) {
+    image.addEventListener('click', e => {
+        
+        previmgId = imgId;        
+        imgId = images.indexOf(image);
+        changeBlur();
+        
         document.getElementById(9).src = document.getElementById(imgId).src;    
     });
 }
